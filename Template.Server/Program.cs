18a402780dfea59ab245f1b1ad509b1b279d2afc
ImTommyDev <tommy.devlpr@gmail.com>
2025-02-20 10:09:03 +0100

    Primer commit limpio

---
+++ Template.Server/Program.cs
@@ -0,0 +1,41 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Facilito la depuración con logs
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
---
+++ template.client/src/App.tsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <h1>Template</h1>
+        </div>
+    );
+}
+
+export default App;
